1e1d4d2ee76bc339ce71b826e28d2af96ffb79d5
ucurtmaavcısı <simsekmerve1717@gmail.com>
2022-01-27 16:37:32 +0300

    çalışma

---
+++ add_up_number.js
@@ -0,0 +1,28 @@
+
+
+// 1.Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10
+
+
+
+// function addup(n) {
+//     sum=0;
+
+//     for (i=0; i<n+1; i++) {
+        
+//         sum+=i;
+//     }
+//     console.log(sum);
+    
+// }
+// addup(4);
+
+// 2.Fix the code in the code tab to pass this challenge (only syntax errors). Look at the examples below to get an idea of what the function should do.
+
+
+// function cubes(num) {
+//  console.log(num**3);
+    
+// }
+// cubes(3);
+    
+
